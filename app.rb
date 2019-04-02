648079e12e1ee4a26d2c21c2f6f9c2e3edf3941d
github username <github email address>
2019-04-02 17:38:50 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,26 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+
+  get '/' do
+    erb :index
+  end
+
+  post '/puppy' do
+    name  = params[:name]
+    age   = params[:age]
+    breed = params[:breed]
+    newpuppy = Puppy.new(name,breed,age)
+    @name = newpuppy.name
+    @age  = newpuppy.months_old
+    @breed = newpuppy.breed
+    erb :display_puppy
+
+  end
+
+   get '/new' do
+
+    erb :create_puppy
+
+  end
 end
